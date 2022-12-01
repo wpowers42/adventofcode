ee01ed5af557bdb9b4ae49991372c7a7a3f6b2fb
Will Powers <wpowers@classdojo.com>
2022-12-01 09:55:30 -0700

    2022 Day 1

---
+++ 2022/Day 01/script.js
@@ -0,0 +1,25 @@
+fs = require('fs')
+
+fs.readFile('2022/Day 01/input.txt', 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+  part1(data);
+});
+
+const part1 = (input) => {
+    let totals = [];
+    let currentTotal = 0;
+
+    input.split('\n').forEach(item => {
+        if (item === '') {
+            totals.push(currentTotal);
+            currentTotal = 0;
+        } else {
+            currentTotal += parseInt(item);
+        }
+    });;
+    totals.sort((a,b) => b - a);
+    console.log(`Part 1: ${totals.slice(0,1)}`);
+    console.log(`Part 2: ${totals.slice(0,3).reduce((acc, val) => acc + val)}`);
+}

f618734a97679c428147fdf8566d19b63d474b61
Will Powers <wpowers@classdojo.com>
2022-12-03 10:15:16 -0700

    2022 Day 3

---
+++ 2022/Day 03/script.js
@@ -0,0 +1,58 @@
+const fs = require('fs');
+const path = require('path');
+
+
+const convertLetterToNumber = letter => {
+    if (letter !== letter.toLowerCase()) {
+        return letter.charCodeAt(0) - 38;
+    } else {
+        return letter.charCodeAt(0) - 96;
+    }
+}
+
+
+const intersection = (setA, setB) => {
+    for (const element of setA.values()) {
+        if (!setB.has(element)) {
+            setA.delete(element);
+        }
+    };
+
+    return setA;
+}
+
+
+const findCommonItem = (words) => {
+    return (words.map(word => new Set(word))
+        .reduce(intersection)
+        .values().next().value);
+}
+
+
+const part1 = (rucksack) => {
+    let compartmentA = rucksack.substr(0, rucksack.length * 0.5);
+    let compartmentB = rucksack.substr(rucksack.length * 0.5);
+    return convertLetterToNumber(findCommonItem([compartmentA, compartmentB]));
+}
+
+
+const part2 = (input) => {
+    let priorityTotal = 0;
+    for (let i = 0; i < input.length; i += 3) {
+        priorityTotal += convertLetterToNumber(findCommonItem((input.slice(i, i + 3))));
+    }
+    return priorityTotal;
+}
+
+
+const solve = (input) => {
+    input = input.split('\n');
+
+    console.log(`Part 1: ${input.map(part1).reduce((acc, val) => acc + val)}`);
+    console.log(`Part 2: ${part2(input)}`);
+}
+
+
+const filePath = path.join(__dirname, "./input.txt");
+const data = fs.readFileSync(filePath, 'utf-8');
+solve(data);

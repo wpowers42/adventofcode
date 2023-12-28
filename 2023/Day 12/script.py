fb7678412ae1ea02cca40ed74badfc779c5c0801
Will Powers <wpowers@classdojo.com>
2023-12-28 21:44:19 +0000

    Day 12 Part 2 WIP

--- 2023/Day 12/script.py
+++ 2023/Day 12/script.py
@@ -1,8 +1,106 @@
-from itertools import permutations, combinations
+import re
+
+
+def pattern(condition: tuple) -> re.Pattern:
+    # condition is a tuple of ints like (1, 1, 3)
+    # return a pattern for this example would be \.?#{1}\.+#{1}\.+#{3}\.?
+
+    # make a pattern for any condition provided in the form of a tuple
+    return re.compile(
+        "(\\.+)?" + "\\.+".join(["#" * num for num in condition]) + "(\\.+)?"
+    )
+
+
+def possible_pattern(condition: tuple) -> re.Pattern:
+    return re.compile(
+        "([.?]+)?"
+        + "[.?]+".join(["[#?]{" + str(num) + "}" for num in condition])
+        + "([.?]+)?"
+    )
+
+
+def is_possibly_valid(pattern: re.Pattern, configuration: str) -> bool:
+    # configuration is a string like ".#...#....###"
+    # return True if the configuration is valid, False otherwise
+    return pattern.match(configuration) is not None
+
+
+def is_complete(configuration: str) -> bool:
+    return configuration.count("?") == 0
+
+
+def is_valid(pattern: re.Pattern, configuration: str) -> bool:
+    # configuration is a string like ".#...#....###"
+    # return True if the configuration is valid, False otherwise
+    return pattern.match(configuration) is not None
+
+
+def solve_line(springs: str, condition: str, repeat: int = 1):
+    springs = "?".join([springs for _ in range(repeat)])
+    condition = ",".join([condition for _ in range(repeat)])
+    condition = tuple(map(int, condition.split(",")))
+
+    context = {
+        "target_num_damaged": sum(condition),
+        # "valid": set(),
+        "num_valid": 0,
+        "target_pattern": pattern(condition),
+        "possible_pattern": possible_pattern(condition),
+        "unknown_indices": [i for i, x in enumerate(springs) if x == "?"],
+    }
+
+    # print(context["possible_pattern"].pattern)
+
+    recursive(springs, springs.count("#"), springs.count("?"), 0, context)
+    print("Valid:", context["num_valid"])
+    return context["num_valid"]
+
+
+def recursive(springs, num_damaged_known, num_unknown, unknown_index, context):
+    target_num_damaged = context["target_num_damaged"]
+    # valid = context["valid"]
+    target_pattern = context["target_pattern"]
+    possible_pattern = context["possible_pattern"]
+    unknown_indices = context["unknown_indices"]
+
+    if num_damaged_known > target_num_damaged:
+        return
+
+    if num_damaged_known + num_unknown < target_num_damaged:
+        return
+
+    if num_unknown == 0:
+        if is_valid(target_pattern, springs):
+            # valid.add(springs)
+            context["num_valid"] += 1
+        return
+
+    if not is_possibly_valid(possible_pattern, springs):
+        # print("not possible", springs)
+        return
+
+    index = unknown_indices[unknown_index]
+    recursive(
+        springs[:index] + "#" + springs[index + 1 :],
+        num_damaged_known + 1,
+        num_unknown - 1,
+        unknown_index + 1,
+        context,
+    )
+    recursive(
+        springs[:index] + "." + springs[index + 1 :],
+        num_damaged_known,
+        num_unknown - 1,
+        unknown_index + 1,
+        context,
+    )
+
+
+from multiprocessing import Pool
 
 
 def main():
-    debug = True
+    debug = False
     file = "example.txt" if debug else "input.txt"
     with open(file) as f:
         input = f.read()
@@ -11,61 +109,51 @@ def main():
 
     # Part 1
     total = 0
-    for index, (spring, condition) in enumerate(records):
-        spring = list(spring)
-        num_possible = possible_springs(spring, condition)
-        total += num_possible
+    for springs, condition in records:
+        total += solve_line(springs, condition, 1)
     print("Part 1:", total)
 
-    # Part 2 - will never finish, need to find a better way
+    # Part 2
+    # total = 0
+    # index = 0
+    # for springs, condition in records[:10]:
+    #     print(index, springs, condition)
+    #     index += 1
+    #     a = solve_line(springs, condition, 1)
+    #     b = solve_line(springs, condition, 2)
+    #     num_possible = a * (b / a) ** 4
+    #
+    #     total += num_possible
+    # print("Part 2:", total)
+
+    # Use multiprocessing
     total = 0
-    for index, (spring, condition) in enumerate(records):
-        spring = list(spring)
-        print(index)
-        spring = list("?".join(["".join(spring) for _ in range(5)]))
-        condition = ",".join([condition for _ in range(5)])
-        num_possible = possible_springs(spring, condition)
-        total += num_possible
-    print("Part 2:", total)
+    with Pool() as pool:  # Adjust the number of processes as needed
+        queue = []
+        for springs, condition in records:
+            a = solve_line(springs, condition, 1)
+            b = solve_line(springs, condition, 2)
 
+            if (b / a).is_integer():
+                num_possible = a * (b / a) ** 4
+                total += num_possible
+            else:
+                queue.append((springs, condition))
 
-def possible_springs(springs, condition):
-    num_damaged = sum(map(int, condition.split(",")))
-    num_damaged_known = springs.count("#")
-    num_damaged_to_place = num_damaged - num_damaged_known
+        print(f"Starting multiprocessing with {len(queue)} records...")
+        results = [
+            pool.apply_async(solve_line, (springs, condition, 5))
+            for springs, condition in queue
+        ]
 
-    unknown_indices = [i for i, x in enumerate(springs) if x == "?"]
-    damaged_placements = combinations(unknown_indices, num_damaged_to_place)
+        for index, result in enumerate(results):
+            try:
+                num_possible = result.get()
+                total += num_possible
+            except Exception as exc:
+                print(f"An exception occurred: {exc}")
 
-    num_possible = 0
-    for placement in damaged_placements:
-        springs_list = springs.copy()
-        for index in placement:
-            springs_list[index] = "#"
-        for index in unknown_indices:
-            if springs_list[index] == "?":
-                springs_list[index] = "."
-        if validate(springs_list, condition):
-            num_possible += 1
-    return num_possible
-
-
-def validate(springs, condition):
-    counter = 0
-    result = []
-    for char in springs:
-        if char == "#":
-            counter += 1
-        elif char == "." and counter != 0:
-            result.append(counter)
-            counter = 0
-    if counter != 0:
-        result.append(counter)
-
-    result = tuple(result)
-
-    condition = tuple(map(int, condition.split(",")))
-    return result == condition
+    print("Total:", total)
 
 
 if __name__ == "__main__":

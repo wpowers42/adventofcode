ec2e68b476288ff1289a64c183800d88dbe7ce96
Will Powers <wpowers@classdojo.com>
2024-12-01 22:50:35 +0000

    2024 Day 01

---
+++ 2024/Day 01/2024_01.sql
@@ -0,0 +1,42 @@
+/* DuckDB */
+create temp table input as
+    (
+        with
+            input as (
+                select *
+                from read_csv('./2024/Day 01/input.csv',
+                              header = false,
+                              columns = {'line':'varchar'})
+            )
+          , split as (
+                select split(line, '   ') as line
+                from input
+            )
+
+        select
+            line[1]::int as a
+          , line[2]::int as b
+        from split
+    );
+
+/* Part 1 */
+with
+    sorted as (
+        select *
+             , row_number() over (order by a) as rn_a
+             , row_number() over (order by b) as rn_b
+        from input
+    )
+
+select sum(abs(a.a - b.b)) as result
+from sorted a
+join sorted b
+    on a.rn_a = b.rn_b
+;
+
+/* Part 2 */
+select distinct sum(a.a * count(b.b)) over () as result
+from input a
+join input b
+    on a.a = b.b
+group by a.a

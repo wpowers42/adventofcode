5f6323036d70761c96dc301f202e26221f24a1ad
Will Powers <wpowers@classdojo.com>
2024-12-08 06:29:25 +0000

    cleanup

--- 2024/Day 08/2024_08.sql
+++ 2024/Day 08/2024_08.sql
@@ -23,73 +23,36 @@ create temp table input as
 set variable max_x = (select max (x) from input);
 set variable max_y = (select max (y) from input);
 
-/* Part 1 */
-with
-    cte as (
-        select
-            x
-          , y
-          , cell
-        from input
-        where cell != '.'
-    )
-  , joined as (
-        select
-            a.x     as x1
-          , a.y     as y1
-          , b.x     as x2
-          , b.y     as y2
-          , y2 - y1 as dy
-          , x2 - x1 as dx
-          , x1 - dx as x3
-          , y1 - dy as y3
-        from cte a
-        join cte b
-            on not (a.x = b.x and a.y = b.y)
-            and a.cell = b.cell
-    )
-
-
-select distinct {'x': x3, 'y': y3} as point
-from joined
-where x3 between 1
-  and getvariable('max_x')
-  and y3 between 1
-  and getvariable('max_y')
-order by 1;
-
-/* Part 2 */
-drop table if exists antennas;
-create temp table antennas as
-    (
-        select
-            x
-          , y
-          , cell
-        from input
-        where cell != '.'
-    );
-
 drop table if exists antenna_pairs;
 create temp table antenna_pairs as
     (
         select
-            a.x      as x1
-          , a.y      as y1
-          , b.x - x1 as dx
-          , b.y - y1 as dy
-        from antennas a
-        join antennas b
+            a.x
+          , a.y
+          , b.x - a.x as dx
+          , b.y - a.y as dy
+        from input a
+        join input b
             on not (a.x = b.x and a.y = b.y)
             and a.cell = b.cell
+        where a.cell != '.'
     );
+
+/* Part 1 */
+
+select count(distinct (x - dx) || ',' || (y - dy)) as result
+from antenna_pairs
+where (x - dx) between 1 and getvariable('max_x')
+  and (y - dy) between 1 and getvariable('max_y');
+
+/* Part 2 */
 
 with
     recursive
-    r(x, y, dx, dy) as (
+    harmonics(x, y, dx, dy) as (
         select
-            x1 as x
-          , y1 as y
+            x
+          , y
           , dx
           , dy
         from antenna_pairs
@@ -99,7 +62,7 @@ with
           , y + dy
           , dx
           , dy
-        from r
+        from harmonics
         where x + dx between 1
             and getvariable('max_x')
           and y + dy between 1
@@ -107,4 +70,4 @@ with
     )
 
 select count(distinct x || ',' || y) as result
-from r;
+from harmonics;

fd146cdd3bc6b4a4bd4f4986f5876ffe130a148c
Will Powers <wpowers@classdojo.com>
2025-01-03 05:02:38 +0000

    2024 Day 22

--- 2024/Day 22/2024_22.sql
+++ 2024/Day 22/2024_22.sql
@@ -16,7 +16,6 @@ create temp table input as
         from cte
     );
 
-
 /* Part 1 */
 with
     recursive
@@ -58,3 +57,65 @@ where round = 2000;
 
 /* Part 2 */
 
+with
+    recursive
+    secrets as (
+        select
+            buyer_id
+          , secret :: bigint as secret
+          , 0                as round
+        from input
+
+        union all
+
+        (
+            with
+                cte as (
+                    select
+                        buyer_id
+                      , xor(secret, secret * 64) % 16777216        as a
+                      , xor(a, floor(a / 32) :: bigint) % 16777216 as b
+                      , xor(b, b * 2048) % 16777216                as secret
+                      , round + 1                                  as round
+                    from secrets
+                )
+
+            select
+                buyer_id
+              , secret
+              , round
+            from cte
+            where round < 2001
+        )
+    )
+
+  , sequences as (
+        select
+            buyer_id
+          , round
+          , secret % 10                                             as bid
+          , lag(bid, 4) over (partition by buyer_id order by round) as bid0
+          , lag(bid, 3) over (partition by buyer_id order by round) as bid1
+          , lag(bid, 2) over (partition by buyer_id order by round) as bid2
+          , lag(bid, 1) over (partition by buyer_id order by round) as bid3
+          , lag(bid, 0) over (partition by buyer_id order by round) as bid4
+          , [bid1 - bid0, bid2 - bid1, bid3 - bid2, bid4 - bid3]    as bids
+        from secrets
+    )
+
+  , valid_sequences as (
+        select
+            buyer_id
+          , bid
+          , bids
+        from sequences
+        where bid0 is not null
+        qualify row_number() over (partition by buyer_id, bids order by round) = 1
+    )
+
+select sum(bid) as result
+from valid_sequences
+group by bids
+order by 1 desc
+limit 1
+;
